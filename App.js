fa0ffdc70eaf27660ae046cae95420d1aeb37489
Nitish Kumar <101048101+nitishkumar80@users.noreply.github.com>
2024-03-29 01:05:06 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,48 +1,7 @@
 
-// import React from 'react';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-// import { MaterialIcons } from '@expo/vector-icons';
 import HomeScreen from './component/Pages/HomeScreen';
 import FavoritesScreen from './component/Pages/FavoritesScreen';
 
-// const Tab = createBottomTabNavigator();
-
-// export default function App() {
-//   return (
-//     <NavigationContainer>
-//       <Tab.Navigator
-//         screenOptions={({ route }) => ({
-//           tabBarIcon: ({ color, size }) => {
-//             let iconName;
-//             if (route.name === 'Home') {
-//               iconName = 'home';
-//             } else if (route.name === 'Favorites') {
-//               iconName = 'favorite';
-//             }
-//             return <MaterialIcons name={iconName} size={size} color={color} />;
-//           },
-//         })}
-//         tabBarOptions={{
-//           activeTintColor: 'blue',
-//           inactiveTintColor: 'gray',
-         
-//         }}
-//       >
-//         <Tab.Screen name="Home" component={HomeScreen}    options={{ headerShown: false }}/>
-//         <Tab.Screen name="Favorites" component={FavoritesScreen} 
-//                   options={{ headerShown: false }}
-//         />
-//       </Tab.Navigator>
-//     </NavigationContainer>
-//   );
-// }
-
-
-
-
-
-// App.js
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
